fd5d4b5841bbce09fd8a591024bc87af4889f891
Denis Developer <denispereiradosreis@gmail.com>
2024-03-10 19:53:33 -0300

    correção url

--- src/01 - Infraestructure/Api.Vendas/Program.cs
+++ src/01 - Infraestructure/Api.Vendas/Program.cs
@@ -12,8 +12,8 @@ namespace Api.Vendas
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
-                    webBuilder.UseStartup<Startup>();
-                              //.UseUrls($"http://0.0.0.0:{port}");
+                    webBuilder.UseStartup<Startup>()
+                              .UseUrls($"http://0.0.0.0:{port}");
                 });
     }
 }
