193812902b34da62c44e57d5746826979c9c8c8f
Denis Developer <denispereiradosreis@gmail.com>
2024-03-10 19:51:24 -0300

    add initial itens in database

--- src/01 - Infraestructure/Data/Configurations/BancosDadosExtentions.cs
+++ src/01 - Infraestructure/Data/Configurations/BancosDadosExtentions.cs
@@ -28,56 +28,9 @@ namespace Application.Configurations.UserMain
             {
                 logDbContext.Database.Migrate();
             }
-            SeedVendasDbContext(vendasDbContext);
+
             PrepararUsuarioInicial(serviceScope);
         }
-
-        private static void SeedVendasDbContext(VendasDbContext context)
-        {
-            if (!context.Vendas.Any())
-            {
-                var random = new Random();
-                var produtos = new List<string>
-        {
-            "Bala", "Pirulito", "Chiclete", "Paçoca", "Chocolate", "Biscoito", "Goma de Mascar",
-            "Bolo", "Cupcake", "Pastilha", "Bombom", "Torrone", "Marshmallow", "Jujuba",
-            "Caramelos", "Trufa", "Brownie", "Cookie", "Muffin", "Macaron",
-            "Pão de Mel", "Brigadeiro", "Beijinho", "Cajuzinho", "Quindim",
-            "Pé de Moleque", "Cocada", "Alfajor", "Doce de Leite", "Gelatina"
-        };
-
-                for (int i = 0; i < 30; i++)
-                {
-                    var preco = Math.Round(random.NextDouble() * (10 - 1) + 1, 2); // Preço entre 1 e 10, com duas casas decim
-                    var quantidade = random.Next(1, 20); // Quantidade vendida entre 1 e 20
-                    var data = RandomDay(); // Data aleatória entre 01/01/2024 e 01/03/2024
-
-                    var venda = new Venda
-                    {
-                        Nome = produtos[random.Next(produtos.Count)],
-                        Preco = preco,
-                        DataVenda = data,
-                        QuantidadeVendido = quantidade,
-                        TotalDaVenda = Math.Round(quantidade * preco, 2)
-                    };
-
-                    context.Vendas.Add(venda);
-                }
-
-                context.SaveChanges();
-            }
-        }
-
-        // Método auxiliar para gerar um DateTime aleatório entre duas datas
-        private static DateTime RandomDay()
-        {
-            var start = new DateTime(2024, 1, 1);
-            var end = new DateTime(2024, 3, 1);
-            var random = new Random();
-            int range = (end - start).Days;
-            return start.AddDays(random.Next(range));
-        }
-
 
         public static void PrepararUsuarioInicial(IServiceScope serviceScope)
         {
@@ -112,6 +65,51 @@ namespace Application.Configurations.UserMain
                     _userManager.AddClaimsAsync(user, claims).Wait();
                 }
             }
+        }
+
+        private static void SeedVendasDbContext(VendasDbContext context)
+        {
+            if (!context.Vendas.Any())
+            {
+                var random = new Random();
+                var produtos = new List<string>
+                {
+                    "Bala", "Pirulito", "Chiclete", "Paçoca", "Chocolate", "Biscoito", "Goma de Mascar",
+                    "Bolo", "Cupcake", "Pastilha", "Bombom", "Torrone", "Marshmallow", "Jujuba",
+                    "Caramelos", "Trufa", "Brownie", "Cookie", "Muffin", "Macaron",
+                    "Pão de Mel", "Brigadeiro", "Beijinho", "Cajuzinho", "Quindim",
+                    "Pé de Moleque", "Cocada", "Alfajor", "Doce de Leite", "Gelatina"
+                };
+
+                for (int i = 0; i < 30; i++)
+                {
+                    var preco = Math.Round(random.NextDouble() * (10 - 1) + 1, 2);
+                    var quantidade = random.Next(1, 20);
+                    var data = RandomDay();
+
+                    var venda = new Venda
+                    {
+                        Nome = produtos[random.Next(produtos.Count)],
+                        Preco = preco,
+                        DataVenda = data,
+                        QuantidadeVendido = quantidade,
+                        TotalDaVenda = Math.Round(quantidade * preco, 2)
+                    };
+
+                    context.Vendas.Add(venda);
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        private static DateTime RandomDay()
+        {
+            var start = new DateTime(2024, 1, 1);
+            var end = new DateTime(2024, 3, 1);
+            var random = new Random();
+            int range = (end - start).Days;
+            return start.AddDays(random.Next(range));
         }
     }
 }
