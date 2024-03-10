455516a62440d69074ee845541b578c9a6089499
Denis Developer <denispereiradosreis@gmail.com>
2024-03-10 15:52:23 -0300

    remove port comment

--- src/01 - Infraestructure/Api.Vendas/Program.cs
+++ src/01 - Infraestructure/Api.Vendas/Program.cs
@@ -11,9 +11,9 @@ namespace Api.Vendas
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    //var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
-                    webBuilder.UseStartup<Startup>();
-                            //  .UseUrls($"http://0.0.0.0:{port}");
+                    var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+                    webBuilder.UseStartup<Startup>()
+                              .UseUrls($"http://0.0.0.0:{port}");
                 });
     }
 }
