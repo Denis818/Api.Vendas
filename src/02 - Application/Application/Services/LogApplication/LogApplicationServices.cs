78b1fbefa7cc9e2f16141921d538c3033b411a7a
Denis Pereira dos Reis <denis.reis@conciliadora.com.br>
2024-03-11 15:31:01 -0300

    corrigindo namespace

---
+++ src/02 - Application/Application/Services/LogApplication/LogApplicationServices.cs
@@ -0,0 +1,19 @@
+﻿using Api.Vendas.Utilities;
+using Application.Interfaces.Services;
+using Domain.Interfaces.Repository;
+using Domain.Models;
+
+namespace Application.Services.Log
+{
+    public class LogApplicationServices(ILogApplicationRepository logRepository) : ILogApplicationServices
+    {
+        private readonly ILogApplicationRepository _logRepository = logRepository;
+
+        public async Task<PagedResult<LogRequest>> GetLogRequests(int paginaAtual, int itensPorPagina)
+            => await Pagination.PaginateResult(_logRepository.GetLogRequest(), paginaAtual, itensPorPagina);
+
+
+        public async Task<PagedResult<LogError>> GetLogErrors(int paginaAtual, int itensPorPagina)
+            => await Pagination.PaginateResult(_logRepository.GetLogErrors(), paginaAtual, itensPorPagina);
+    }
+}
