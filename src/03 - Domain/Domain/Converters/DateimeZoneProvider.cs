a4ad4730c529405ad35e006d71d45d247d85466f
Denis Developer <denispereiradosreis@gmail.com>
2024-03-11 08:35:23 -0300

    corrigindo date

--- src/03 - Domain/Domain/Converters/DateimeZoneProvider.cs
+++ src/03 - Domain/Domain/Converters/DateimeZoneProvider.cs
@@ -1,9 +1,19 @@
-﻿namespace Domain.Converters
+﻿using System.Globalization;
+
+namespace Domain.Converters
 {
     public class DateimeZoneProvider
     {
         public static DateTime GetBrasiliaTimeZone(DateTime dateTime)
-            => TimeZoneInfo.ConvertTimeFromUtc(dateTime,
-                TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
+        {
+            TimeZoneInfo brasiliaZone = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"); //horário de brasilia
+            DateTime brasiliaDateTime = TimeZoneInfo.ConvertTimeFromUtc(dateTime, brasiliaZone);
+
+            // Configura a cultura do thread corrente para pt-BR
+            CultureInfo.CurrentCulture = new CultureInfo("pt-BR");
+            CultureInfo.CurrentUICulture = new CultureInfo("pt-BR");
+
+            return brasiliaDateTime;
+        }
     }
 }
