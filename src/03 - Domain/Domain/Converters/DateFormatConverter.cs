26e7575df7ce31166ddf474502fcb66988104edc
Denis Pereira dos Reis <denis.reis@conciliadora.com.br>
2024-03-11 10:08:41 -0300

    correção no formato da data

--- src/03 - Domain/Domain/Converters/DateFormatConverter.cs
+++ src/03 - Domain/Domain/Converters/DateFormatConverter.cs
@@ -6,22 +6,14 @@ namespace Domain.Converters
 {
     public class DateFormatConverter : JsonConverter<DateTime>
     {
-        private const string DateFormat = "dd/MM/yyyy";
-
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            var dateString = reader.GetString();
-            if (dateString == null)
-            {
-                throw new InvalidOperationException("Cannot convert null or empty string to DateTime.");
-            }
-
-            return DateTime.ParseExact(dateString, DateFormat, CultureInfo.InvariantCulture);
+            return DateTime.Parse(reader.GetString());
         }
 
         public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue(value.ToString("g", CultureInfo.InvariantCulture));
+            writer.WriteStringValue(value.ToString("g", new CultureInfo("pt-BR")));
         }
     }
 }
--- src/03 - Domain/Domain/Converters/DateimeZoneProvider.cs
+++ src/03 - Domain/Domain/Converters/DateimeZoneProvider.cs
@@ -1,19 +1,9 @@
-﻿using System.Globalization;
-
-namespace Domain.Converters
+﻿namespace Domain.Converters
 {
     public class DateimeZoneProvider
     {
         public static DateTime GetBrasiliaTimeZone(DateTime dateTime)
-        {
-            TimeZoneInfo brasiliaZone = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"); //horário de brasilia
-            DateTime brasiliaDateTime = TimeZoneInfo.ConvertTimeFromUtc(dateTime, brasiliaZone);
-
-            // Configura a cultura do thread corrente para pt-BR
-            CultureInfo.CurrentCulture = new CultureInfo("pt-BR");
-            CultureInfo.CurrentUICulture = new CultureInfo("pt-BR");
-
-            return brasiliaDateTime;
-        }
+            => TimeZoneInfo.ConvertTimeFromUtc(dateTime,
+                TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time"));
     }
 }
